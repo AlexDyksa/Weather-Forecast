cdbdfd16b8251d58990a468c763ff4b9ff2b4050
Alex Dyksa <odyksa@myagent.com.ua>
2019-03-27 12:51:29 +0200

    added components Form and Outcomes

---
+++ src/components/Form/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Form = () => (
+    <form className="app__form">
+        <input type="text" className="app__search" />
+        <button className="app__submit">Get Data</button>
+    </form>
+);
+
+export default Form;
--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
 
 import './styles.css';
+import Form from '../Form';
+import Outcomes from '../Outcomes';
 
 const Main = () => (
     <main className="main">
-        1
+        <Form />
+        <Outcomes />
     </main>
 );
 
