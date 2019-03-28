abc06be7aca5993badbed705124c0d5a3b69c343
Alex Dyksa <odyksa@myagent.com.ua>
2019-03-28 10:34:39 +0200

    modified Form component and container

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -1,8 +1,17 @@
 import React from 'react';
 
-const Form = () => (
-    <form className="app__form">
-        <input type="text" className="app__search" placeholder="Enter city name" />
+const Form = (props) => (
+    <form
+        className="app__form"
+        onSubmit={e => e.preventDefault()}
+    >
+        <input
+            type="text"
+            className="app__search"
+            placeholder="Enter city name"
+            value={props.enteredCityName}
+            onChange={e => props.changeCityName(e.target.value)}
+        />
         <button className="app__submit">Get Forecast</button>
     </form>
 );
--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import './styles.css';
-import Form from '../Form';
+import Form from '../../containers/formContainer';
 import Outcomes from '../Outcomes';
 
 const Main = () => (
