3bba5d0d3316c168ecb97a142bb616524a4a2bc5
Alex Dyksa <odyksa@myagent.com.ua>
2019-03-28 13:52:08 +0200

    added getWeather method to Form component

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -3,7 +3,24 @@ import React, { Component } from 'react';
 class Form extends Component {
     handleSubmitForm = e => {
         e.preventDefault();
+        this.getWeatherData();
     };
+
+    getWeatherData = () => {
+        const API_KEY = '7122cd64b0ae03295d684f0a0554f2b9';
+        const API_URL = `http://api.openweathermap.org/data/2.5/weather?q=${this.props.enteredCityName}&units=metric&appid=${API_KEY}`;
+
+        fetch(API_URL)
+            .then(res => res.json())
+            .then(data => ({
+                city: data.name,
+                temp: data.main.temp,
+                sunrise: data.sys.sunrise,
+                sunset: data.sys.sunset
+            }))
+            .then(weatherForecast => this.props.fetchData(weatherForecast));
+    };
+
 
     handleChangeCityName = e => {
         this.props.changeCityName(e.target.value);
