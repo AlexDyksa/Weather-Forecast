c535596acf884bc99ad94712efb7b1d5f783e6ce
Alex Dyksa <odyksa@myagent.com.ua>
2019-03-28 15:06:05 +0200

    modified component Outcomes

--- src/components/Outcomes/index.js
+++ src/components/Outcomes/index.js
@@ -1,7 +1,16 @@
 import React from 'react';
 
-const Outcomes = () => (
-    <div className="app__outcomes">Outcomes</div>
-);
+const Outcomes = props => {
+    const { city, temp, sunrise, sunset } = props.data;
+
+    return (
+        <div className="app__outcomes">
+            <p>City: {city}</p>
+            <p>Temperature: {temp}</p>
+            <p>Sunrise: {sunrise}</p>
+            <p>Sunset: {sunset}</p>
+        </div>
+    );
+};
 
 export default Outcomes;
