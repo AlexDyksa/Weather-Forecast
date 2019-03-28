bc6bbe6ca81b839d6040554cb338877db8dd4c24
Alex Dyksa <odyksa@myagent.com.ua>
2019-03-28 13:53:25 +0200

    added fetchData

--- src/containers/formContainer.js
+++ src/containers/formContainer.js
@@ -2,14 +2,15 @@ import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 
 import Form from '../components/Form';
-import { changeCityName } from '../actions/formActions';
+import { changeCityName, fetchData } from '../actions/formActions';
 
 const mapStateToProps = state => ({
     enteredCityName: state.form.enteredCityName
 });
 
 const mapDispatchToProps = dispatch => ({
-    changeCityName: bindActionCreators(changeCityName, dispatch)
+    changeCityName: bindActionCreators(changeCityName, dispatch),
+    fetchData: bindActionCreators(fetchData, dispatch)
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(Form);
