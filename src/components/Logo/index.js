52171176f05ec8e1a4cf7a205584a6c45f184fd8
Alex Dyksa <odyksa@myagent.com.ua>
2019-03-29 11:06:51 +0200

    modified logo component

--- src/components/Logo/index.js
+++ src/components/Logo/index.js
@@ -3,7 +3,14 @@ import React from 'react';
 import './styles.css';
 
 const Logo = () => (
-    <div className="logo"></div>
+    <div className="logo">
+        Weather Forecast
+        <div className="logo__icons">
+            <i className="fas fa-cloud"></i>
+            <i className="fas fa-bolt"></i>
+            <i className="fas fa-sun"></i>
+        </div>
+    </div>
 );
 
 export default Logo;
