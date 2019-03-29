a5b97a8c49b074c7dadb34998db3f0053a571e85
Alex Dyksa <odyksa@myagent.com.ua>
2019-03-29 13:12:27 +0200

    modified form and main component

--- src/components/Outcomes/index.js
+++ src/components/Outcomes/index.js
@@ -4,39 +4,50 @@ import FlipMove from 'react-flip-move';
 import './styles.css';
 
 const Outcomes = props => {
-    const { code, city, temp, sunrise, sunset } = props.data;
+    const { code, city, temp, sunrise, sunset, wind, isVisibleOutcomes } = props.data;
 
     return (
-        <FlipMove duration={150} easing="ease-out">
-            <div className="app__outcomes outcomes">
-                <div className="outcomes__title">
-                    Weather in
-                    <span>{city}</span>,
-                    {code}
-                </div>
-                <div className="outcomes__row">
-                    <div className="outcomes__icon">
-                        <i className="fas fa-thermometer-three-quarters"></i>
+        <React.Fragment> {isVisibleOutcomes &&
+            <FlipMove duration={1150} easing="ease-out">
+                <div className="app__outcomes outcomes">
+                    <div className="outcomes__title">
+                        Weather in
+                        <span>{city}</span>,
+                        {code}
                     </div>
-                    Temperature:
-                    <span>{temp}</span>
-                </div>
-                <div className="outcomes__row">
-                    <div className="outcomes__icon">
-                        <i className="fas fa-sun"></i>
+                    <div className="outcomes__row">
+                        <div className="outcomes__icon">
+                            <i className="fas fa-thermometer-three-quarters"></i>
+                        </div>
+                        Temperature:
+                        <span>{temp}</span>
+                        &#8451;
                     </div>
-                    Sunrise:
-                    <span>{sunrise}</span>
-                </div>
-                <div className="outcomes__row">
-                    <div className="outcomes__icon">
-                        <i className="fas fa-sun"></i>
+                    <div className="outcomes__row">
+                        <div className="outcomes__icon">
+                            <i className="fas fa-sun"></i>
+                        </div>
+                        Sunrise:
+                        <span>{sunrise}</span>
                     </div>
-                    Sunset:
-                    <span> {sunset}</span>
+                    <div className="outcomes__row">
+                        <div className="outcomes__icon">
+                            <i className="fas fa-sun"></i>
+                        </div>
+                        Sunset:
+                        <span>{sunset}</span>
+                    </div>
+                    <div className="outcomes__row">
+                        <div className="outcomes__icon">
+                            <i className="fas fa-wind"></i>
+                        </div>
+                        Wind Speed:
+                        <span> {wind}</span>
+                        m/s
+                    </div>
                 </div>
-            </div>
-        </FlipMove>
+            </FlipMove>}
+        </React.Fragment>
     );
 };
 
