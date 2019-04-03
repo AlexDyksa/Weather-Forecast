5026611e01736878c45ec780da34dccab9901224
Alex Dyksa <odyksa@myagent.com.ua>
2019-04-03 14:48:53 +0300

    uninstall react-flip-move

--- src/components/Outcomes/index.js
+++ src/components/Outcomes/index.js
@@ -10,45 +10,43 @@ const Outcomes = props => {
         <React.Fragment>
             {error && <p className="error">Incorrect City Name :-(</p>}
             {city &&
-                <FlipMove duration={1150} easing="ease-out">
-                    <div className="app__outcomes outcomes">
-                        <div className="outcomes__title">
-                            Weather in
-                            <span>{city}</span>,
-                            {code}
-                        </div>
-                        <div className="outcomes__row">
-                            <div className="outcomes__icon">
-                                <i className="fas fa-thermometer-three-quarters"></i>
-                            </div>
-                            Temperature:
-                            <span>{temp}</span>
-                            &#8451;
-                        </div>
-                        <div className="outcomes__row">
-                            <div className="outcomes__icon">
-                                <i className="fas fa-sun"></i>
-                            </div>
-                            Sunrise:
-                            <span>{sunrise}</span>
-                        </div>
-                        <div className="outcomes__row">
-                            <div className="outcomes__icon">
-                                <i className="fas fa-sun"></i>
-                            </div>
-                            Sunset:
-                            <span>{sunset}</span>
-                        </div>
-                        <div className="outcomes__row">
-                            <div className="outcomes__icon">
-                                <i className="fas fa-wind"></i>
-                            </div>
-                            Wind Speed:
-                            <span> {wind}</span>
-                            m/s
-                        </div>
+                <div className="app__outcomes outcomes">
+                    <div className="outcomes__title">
+                        Weather in
+                        <span>{city}</span>,
+                        {code}
                     </div>
-                </FlipMove>
+                    <div className="outcomes__row">
+                        <div className="outcomes__icon">
+                            <i className="fas fa-thermometer-three-quarters"></i>
+                        </div>
+                        Temperature:
+                        <span>{temp}</span>
+                        &#8451;
+                    </div>
+                    <div className="outcomes__row">
+                        <div className="outcomes__icon">
+                            <i className="fas fa-sun"></i>
+                        </div>
+                        Sunrise:
+                        <span>{sunrise}</span>
+                    </div>
+                    <div className="outcomes__row">
+                        <div className="outcomes__icon">
+                            <i className="fas fa-sun"></i>
+                        </div>
+                        Sunset:
+                        <span>{sunset}</span>
+                    </div>
+                    <div className="outcomes__row">
+                        <div className="outcomes__icon">
+                            <i className="fas fa-wind"></i>
+                        </div>
+                        Wind Speed:
+                        <span> {wind}</span>
+                        m/s
+                    </div>
+                </div>
             }
         </React.Fragment>
     );
